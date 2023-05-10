6d20a82eb4348b066b59f02260ce68c2bdac0848
JL <jblai083@uottawa.ca>
2023-05-10 14:14:28 -0400

    API to get the half life from Expasy

---
+++ ProtParam.py
@@ -0,0 +1,25 @@
+import requests
+import re
+
+
+URL = 'https://web.expasy.org/cgi-bin/protparam/protparam'
+def get_sequence_half_life(url, sequence):
+    payload = {
+        'sequence' : sequence
+    }
+    response = requests.post(url, payload)
+
+    if response.status_code == 200:
+
+        html  = response.text
+
+        # extract the half life from html 
+        pattern = r"half-life is:\s?(\d+(\.\d+)?) hours"
+        match = re.search(pattern, html)
+        if match:
+            return match.group(1) 
+        else:
+            return None
+    else :
+        print("Request failed with status code:", response.status_code)
+        return None
