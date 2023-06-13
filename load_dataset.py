b585271d6d80fd1b709bcbc070971da012c4933e
amyotjl <jblai083@uottawa.ca>
2023-06-13 10:26:29 -0400

    dataloaders changes and orthogonal dataloader and dataset

--- load_dataset.py
+++ load_dataset.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import numpy as np
 import joblib
-from torch.utils.data import DataLoader, Subset
+from torch.utils.data import DataLoader
 from sklearn.model_selection import train_test_split
 import torch
-import itertools
+from utils import one_hot_encode, aa_matrix_encoded, mul_random_matrix
 
 
 # "data/AminoAcid.xlsx"
-
 def load_amino_acid(path):
     #loading file
     amino_acid_df = pd.read_excel(path, header=None)
@@ -24,19 +23,7 @@ def load_amino_acid(path):
 
     amino_acid_df['seq_one_hot'] = amino_acid_df['sequence'].apply(lambda seq: one_hot_encode(seq, max_length_amino_acid, unique_amino_acid))
     return amino_acid_df
-# Function to one-hot encode a sequence of amino acid.
-# The output is a matrix of max_length_amino_acid (1965) x unique_amino_acid (21). Sequence shorter than max_length_amino_acid are filled with 0. 
-def one_hot_encode(seq, max_length, unique, padding_char = None):
-    if padding_char != None:
-        seq = seq.ljust(max_length, padding_char)
-        unique.append(padding_char)
-    matrix = np.zeros((max_length, len(unique)))
-    for idx, elem in enumerate(seq):
-        matrix[idx][unique.index(elem)] = 1
-    return matrix.astype(np.float32)
 
-
-#@title Read in file in fasta format. { display-mode: "form" }
 def read_fasta( fasta_path, split_char="!", id_field=0):
     '''
         Reads in fasta file containing multiple sequences.
@@ -59,37 +46,75 @@ def read_fasta( fasta_path, split_char="!", id_field=0):
                 # repl. all whie-space chars and join seqs spanning multiple lines, drop gaps and cast to upper-case
                 seq= ''.join( line.split() ).upper().replace("-","")
                 # repl. all non-standard AAs and map them to unknown/X
-                seq = seq.replace('U','X').replace('Z','X').replace('O','X')
+                # seq = seq.replace('U','X').replace('Z','X').replace('O','X')
                 seqs[ uniprot_id ] += seq 
 
     return seqs
 
-
 def load_embeddings(path):
     return joblib.load(path)
 
-def get_dataloader(path, batch_size = 64, shuffle = True, train_size = 1):
+def get_dataloader(path, AA_random_matrices, batch_size = 64, shuffle = True, train_size = 1.0, num_workers = 0):
+    print(' Loading embeddings')
     embeddings_dict = load_embeddings(path)
+    print(' Handling embeddings')
     sequences = [val['sequence'] for val in embeddings_dict.values()]
+    print(' Sequences done')
     longest_sequence = len(max(sequences, key = len))
-    unique_amino_acid = list(set([v for val in embeddings_dict.values() for v in list(val['sequence'])]))
-    unique_amino_acid.sort()
-    one_hots = [one_hot_encode(seq, longest_sequence, unique_amino_acid, padding_char='X') for seq in sequences]
-    embbedings = torch.Tensor([[val['emb']] for val in embeddings_dict.values()])
-    del embeddings_dict
-    ground_truths = np.array(one_hots)
-    dataset = list(zip(embbedings, ground_truths))
-    train_indices, test_indices, _, _ = train_test_split(
-                                            range(len(dataset)),
-                                            dataset,
-                                            train_size=train_size,
-                                        )
+    print(' Found longest sequence: {}'.format(longest_sequence))
 
-    train_split = Subset(dataset, train_indices)
-    test_split = Subset(dataset, test_indices)
+    one_hots = [mul_random_matrix(seq, longest_sequence, AA_random_matrices) for seq in sequences]
+    print(' Created the label matrices')
+    embbedings = [torch.from_numpy(val['emb']).unsqueeze(dim=0) for val in embeddings_dict.values()]
+    print(' Embeddings done')
 
-    train = DataLoader(train_split, batch_size =  batch_size, shuffle = shuffle)
-    test = DataLoader(test_split, batch_size =  batch_size, shuffle = shuffle)
+    if train_size != 1.0:
+        train_embs, test_embs, train_labels, test_labels = train_test_split(embbedings,one_hots,train_size=train_size)
+        train_dataset = list(zip(train_embs, train_labels))
+        train = DataLoader(train_dataset, batch_size =  batch_size, shuffle=shuffle, num_workers=num_workers)
+        
+        test_dataset = list(zip(test_embs, test_labels))
+        test = DataLoader(test_dataset, batch_size =  batch_size,shuffle=shuffle, num_workers=num_workers)
+    else:
 
+        train_dataset = list(zip(embbedings, one_hots))
+        train = DataLoader(train_dataset, batch_size =  batch_size, shuffle=shuffle, num_workers=num_workers)
+        test = None
     return train, test
+
+def get_orth_dataloader(path, batch_size = 64, shuffle = True, train_size = 0.8, num_workers = 0):
+
+    data = joblib.load(path)
+    inputs = data['inputs']
+    sequences = data['sequences']
+
+    if train_size != 1.0:
+        train_inputs, test_inputs, train_seqs, test_seqs = train_test_split(
+                                                inputs,
+                                                sequences,
+                                                train_size=train_size,
+                                            )
+        train_dataset = list(zip(train_inputs, train_seqs))
+        train = DataLoader(train_dataset, batch_size =  batch_size, shuffle=shuffle, num_workers=num_workers)
+        
+        test_dataset = list(zip(test_inputs, test_seqs))
+        test = DataLoader(test_dataset, batch_size =  batch_size,shuffle=shuffle, num_workers=num_workers)
+    else:
+
+        train_dataset = list(zip(inputs,sequences))
+        train = DataLoader(train_dataset, batch_size =  batch_size, shuffle=shuffle, num_workers=num_workers)
+        test = None
+    return train, test
+
+
+def create_orth_dataset(fasta_path, random_orth_matrices_path, longuest_seq, output_file):
+    fasta = read_fasta(fasta_path)
+    orth_matrix = joblib.load(random_orth_matrices_path)
+    seqs = []
+    inputs = []
+    for seq in fasta.values():
+        inputs.append(aa_matrix_encoded(seq, longuest_seq, orth_matrix, '_'))
+        seqs.append(seqs)
+
+    joblib.dump({"input":torch.stack(inputs), "sequence":seqs}, output_file)
 
